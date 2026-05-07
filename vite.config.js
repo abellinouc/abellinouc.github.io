2fdfa96c055a62b384454c7ddd13a8776dc6d866
gbarrueto <gbarrueto@uc.cl>
2026-05-07 13:23:29 -0400

    no SSL

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
-  plugins: [svelte(), basicSsl()],
+  plugins: [svelte()],
   server: {
     host: true,
-    https: true,
     watch: {
       ignored: ["**/data/bigdata/**", "**/data/smalldata/**"],
     },
