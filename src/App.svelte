b2f9ca6ee01e8666167e17670c292c9b500b4c27
gbarrueto <gbarrueto@uc.cl>
2026-05-08 15:39:34 -0400

    disable reset 3

--- src/App.svelte
+++ src/App.svelte
@@ -244,7 +244,7 @@
     let targetLogFov = logFov;
     const ZOOM_SMOOTHING = 0.12;
     let zoomAnimating = false;
-    const ZOOM_IDLE_TIMEOUT = 99999999999; // set high by now
+    const ZOOM_IDLE_TIMEOUT = 9999999; // set high by now
     const ZOOM_MOTION_THRESHOLD = 0.015;
     let zoomIdleTimer = null;
     let lastZoomMotion = null;
